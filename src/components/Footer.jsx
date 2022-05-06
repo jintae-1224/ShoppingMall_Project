d117bc9c06ab1dc9d1d37971c2f9cfbff99d018e
JinTae <kab7210@naver.com>
2022-05-06 15:01:20 +0900

    Footer && Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
-export default function Footer(){
-    return <div>
-        a
-    </div>
-}
+export default function Footer() {
+    return (
+      <footer>
+        <p>© 2021 Mark Lee. All rights reserved.</p>
+      </footer>
+    );
+  }
